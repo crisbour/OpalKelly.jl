314bd6e8645d24601039563d7d55d43ec1e50e61
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-12-25 10:47:39 +0000

    Fixup finalizer

--- src/types.jl
+++ src/types.jl
@@ -11,24 +11,32 @@ mutable struct FPGA <: IC
 end
 
 function FPGA(bitfile;usb3=false)
-    board = Ptr{Nothing}(1)
-    mylib = Ptr{Nothing}(1)
-    FPGA(0,
-         board,
-         mylib,
-         bitfile,
-         #zeros(UInt8, USB_BUFFER_SIZE),
-         zeros(UInt8, 0),
-         usb3,
-         0)
+  board = Ptr{Nothing}(0)
+  mylib = Ptr{Nothing}(0)
+  fpga = FPGA(0,
+       board,
+       mylib,
+       bitfile,
+       #zeros(UInt8, USB_BUFFER_SIZE),
+       zeros(UInt8, 0),
+       usb3,
+       0)
+  finalizer(cleanup, fpga)
+  fpga
 end
 
-finalizer(FPGA) do fpga
+# FPGA finalizer
+function cleanup(fpga::FPGA)
   @info "Cleaning up FPGA connection to OK FrontPanel"
-  if is_open(fpga)
-    close(fpga)
-  else
-    @warn "FPGA was not open. Program might have exited unexpectedly!"
+  if fpga.lib != C_NULL
+    if is_open(fpga)
+      close(fpga)
+    else
+      @warn "FPGA was not open. Program might have exited unexpectedly!"
+    end
+    destruct(fpga)
+    Libdl.dlclose(fpga.lib)
+    @info "FPGA desctructed! => Safe exit"
   end
-  destruct(fpga)
 end
+
